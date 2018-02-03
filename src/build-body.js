81b00df793a46db74896d23fc169bee15697a1fc
zacanger <zac@zacanger.com>
2018-02-03 13:36:03 -0700

    Add twitter client widget i guess

--- src/build-body.js
+++ src/build-body.js
@@ -24,6 +24,9 @@ const buildBody = async () => {
   <html lang="en">
   <head>
     <title>Loonaverse</title>
+    <meta name="author" content="Zac Anger">
+    <meta name="description" content="Social post aggregator for Loona content">
+    <meta name="keywords" content="loona">
     <meta charset="utf-8">
   </head>
   <body style="display: flex; flex-direction: row;">
@@ -33,7 +36,8 @@ const buildBody = async () => {
     </section>
     <section>
       <h1>Twitter</h1>
-      <span>Twitter content will go here</span>
+        <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/hashtag/loona" data-widget-id="959887584640958464">#loona Tweets</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     </section>
   </body>
   </html>
