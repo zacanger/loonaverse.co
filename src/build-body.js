6ce9cb1207481865ff365d3f8aa31798b457d823
zacanger <zac@zacanger.com>
2018-02-03 13:45:16 -0700

    Add favicon

--- src/build-body.js
+++ src/build-body.js
@@ -27,6 +27,7 @@ const buildBody = async () => {
     <meta name="author" content="Zac Anger">
     <meta name="description" content="Social post aggregator for Loona content">
     <meta name="keywords" content="loona">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
     <meta charset="utf-8">
   </head>
   <body style="display: flex; flex-direction: row;">
