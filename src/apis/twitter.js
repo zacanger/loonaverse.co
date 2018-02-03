5fa9e59b9f6f30119fccd9e51d7e32fa891eee8c
zacanger <zac@zacanger.com>
2018-02-03 12:09:02 -0700

    Add streaming?

--- src/apis/twitter.js
+++ src/apis/twitter.js
@@ -7,3 +7,13 @@ client.get('search/tweets', { q: 'loona' }, (err, tweets) => {
   if (err) throw err
   console.log(tweets)
 })
+
+client.stream('statuses/filter', { track: 'loona' }, (stream) => {
+  stream.on('data', (tweet) => {
+    console.log(tweet)
+  })
+
+  stream.on('error', (err) => {
+    throw err
+  })
+})
