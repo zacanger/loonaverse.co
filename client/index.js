a01bf82e9985da2b2c4135582ea508b43af5a846
zacanger <zac@zacanger.com>
2018-02-10 15:38:35 -0700

    update automatically

--- client/index.js
+++ client/index.js
@@ -2,23 +2,44 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Card from './card'
 
+const time1minute = 60 * 1000
+const time5minutes = time1minute * 5
+
 class App extends Component {
+  interval
+
   state = {
-    posts: []
+    allPosts: [],
+    displayPosts: []
   }
 
+  static platforms = [ 'twitter', 'tumblr' ]
+
   componentWillMount () {
+    this.update()
+  }
+
+  update = () => {
     window.fetch('/posts.json')
       .then((r) => r.json())
-      .then((posts) => {
-        this.setState({ posts })
+      .then((allPosts) => {
+        this.setState({ allPosts, displayPosts: allPosts })
       })
   }
 
+  componentDidMount () {
+    this.interval = setInterval(this.update, time5minutes)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.interval)
+  }
+
   render () {
+    const { displayPosts } = this.state
     return (
       <section>
-        {this.state.posts.map((post) => <Card key={post.id} {...post} />)}
+        {displayPosts.map((post) => <Card key={post.id} {...post} />)}
       </section>
     )
   }
