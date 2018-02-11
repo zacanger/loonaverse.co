59bf6b77906171948dce4906b820e1eee546501e
zacanger <zac@zacanger.com>
2018-02-11 12:56:14 -0700

    Cluster.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-require('./server')
+const cluster = require('cluster')
+const { cpus } = require('os')
+
+const procs = cpus().length
+
+if (cluster.isMaster) {
+  for (let i = 0; i < procs; i++) {
+    cluster.fork()
+  }
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died! forking.`)
+    cluster.fork()
+  })
+} else {
+  require('./server')
+}
