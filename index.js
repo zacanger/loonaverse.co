3d70ba8b371a067b34d575ad60b2cb7d4f49aaca
zacanger <zac@zacanger.com>
2018-02-16 23:15:16 -0700

    Some updates

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const cluster = require('cluster')
-const { cpus } = require('os')
-
-const procs = cpus().length
-
-if (cluster.isMaster) {
-  for (let i = 0; i < procs; i++) {
-    cluster.fork()
-  }
-  cluster.on('exit', (worker) => {
-    console.log(`worker ${worker.process.pid} died! forking.`)
-    cluster.fork()
-  })
-} else {
-  require('./server')
-}
+const cluster = require('boring-cluster')
+const { resolve } = require('path')
+cluster(resolve(__dirname, 'server'))
